f953e7ba56709c9619d52ca8ce8b99202589c2f1
samusamudra <141795732+samudsamudra@users.noreply.github.com>
2025-03-28 06:21:22 +0000

    Update index.ts

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./config";
 
-// Jalankan NextAuth dengan config lo
 const handler = NextAuth(authConfig);
 
-// Ekspor GET dan POST dari handler, jangan destructure!
 export const GET = handler.GET;
 export const POST = handler.POST;
